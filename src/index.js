97eb434909018148bc8c9f0961e9db60fc6cdc13
Denis <dean.angell@mail.ru>
2021-02-10 20:41:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [],
+        open = [],
+        map = new Map(),
+        sameArr = new Set(),
+        sameSet = new Set();
+
+    for (let i = 0; i < bracketsConfig.length; i++){
+        open.push(bracketsConfig[i][0]);
+        map.set(bracketsConfig[i][0],bracketsConfig[i][1]);
+        if (bracketsConfig[i][0] == bracketsConfig[i][1]){
+            sameArr.add(bracketsConfig[i][0]);
+        }
+    }
+
+    for (let i = 0; i<str.length; i++){
+        if(sameArr.has(str[i])){
+            if(sameSet.has(str[i])){
+                let temp = stack.pop();
+                sameSet.delete(str[i]);
+                if (str[i] === map.get(temp)){
+                    continue;
+                }else{
+                    return false;
+                }
+            }else{
+                stack.push(str[i]);
+                sameSet.add(str[i]);
+            }
+        }else if (open.includes(str[i])){
+            stack.push(str[i]);
+        }else{
+            let temp = stack.pop();
+            if (str[i] === map.get(temp)){
+                continue;
+            }else{
+                return false;
+            }
+        }
+    }
+    if (stack.length !== 0){
+        return false;
+    }else{
+        return true;
+    }
+};
